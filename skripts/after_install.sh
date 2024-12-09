ed9f10bcda13e69064af31f0f827c7f0c509d36e
Toxa333 <litvin.tolick@gmail.com>
2024-12-09 10:56:54 +0200

    Update after_install.sh

--- skripts/after_install.sh
+++ skripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Останавливаем старое приложение
-docker stop repositorykr || true
-docker rm repositorykr || true
+#docker stop repositorykr || true
+#docker rm repositorykr || true
 
 # Запускаем новое приложение
 docker run -d --name repositorykr -p 80:8080 484907488753.dkr.ecr.us-east-1.amazonaws.com/repositorykr:latest

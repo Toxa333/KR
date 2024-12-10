058d4b272f2a17ee405b63dedb08e6a05b72767a
Toxa333 <litvin.tolick@gmail.com>
2024-12-10 20:19:30 +0200

    Update after_install.sh

--- skripts/after_install.sh
+++ skripts/after_install.sh
@@ -5,4 +5,4 @@ docker stop KRDeploy || true
 docker rm KRDeploy || true
 
 # Запускаем новое приложение
-docker run -d --name KRDeploy -p 80:8080 123456789012.dkr.ecr.us-east-1.amazonaws.com/repositorykr:latest
+docker run -d --name KRDeploy -p 80:8080 484907488753.dkr.ecr.us-east-1.amazonaws.com/repositorykr:latest

b30b5bf705f68d58d2d2c4308295edc74280e4d9
Toxa333 <litvin.tolick@gmail.com>
2024-12-15 19:57:31 +0200

    Update after_install.sh

--- skripts/after_install.sh
+++ skripts/after_install.sh
@@ -6,4 +6,5 @@ docker rm KRDeploy || true
 
 
 # Запускаем новое приложение
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 484907488753.dkr.ecr.us-east-1.amazonaws.com
 sudo docker run -d --name KRDeploy -p 80:8080 484907488753.dkr.ecr.us-east-1.amazonaws.com/repositorykr:latest
